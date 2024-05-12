05acc69f8cdc4e2713de2d538d4933c14d5cba62
rootblind <blindcatalin@protonmail.com>
2024-05-12 17:40:14 +0300

    Cart ticket models

---
+++ ASP.NET/PlaneTickets/Models/Cart.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PlaneTickets.Models
+{
+    public class Cart
+    {
+        [Key]
+        public int CartId { get; set; }  // Primary key
+
+        public int UserId { get; set; }  // Foreign key to the Users table
+        public User User { get; set; }  // Might be needed for additional user data
+
+        public decimal TotalPrice { get; set; }
+
+        // ICollection<CartItem> is used for the relationship between ShoppingCart and CartItem
+        public ICollection<CartTicket> CartItems { get; set; }
+    }
+}
---
+++ ASP.NET/PlaneTickets/Models/CartTicket.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PlaneTickets.Models
+{
+    public class CartTicket
+    {
+        [Key]
+        public int CartTicketId { get; set; }  // Primary key
+
+        public int CartId { get; set; }  // Foreign key to the ShoppingCart table
+        public Cart ShoppingCart { get; set; }  // Might be needed for additional shopping cart data
+
+        public int TicketId { get; set; }  // Foreign key to the Products table
+        public Ticket Ticket { get; set; }  // Might be needed for additional product data
+
+        public int Quantity { get; set; }
+    }
+}
