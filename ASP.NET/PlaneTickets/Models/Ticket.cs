bbda9e5392f3b55085b2f14457a7508483edbda5
Wisuy <legaluni@gmail.com>
2024-05-12 17:09:13 +0300

    Ver1.0

---
+++ ASP.NET/PlaneTickets/Models/Ticket.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
+namespace PlaneTickets.Models
+{
+    public class Ticket
+    {
+        [Key]
+        public int TicketId { get; set; }
+        public required string Departure { get; set; }
+        public required string Destination { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public required string Class { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public DateTime DestinationTime { get; set; }
+        public required string DestinationGate { get; set; }
+        public required string DepartureGate { get; set; }
+    }
+}
